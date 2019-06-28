8ec26dd3254a930ef2e9e9c0de2e0601f30ef2a5
Waataru Takahashi <universe3523@gmail.com>
2019-06-27 22:42:26 -0700

    little change in test file

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pandas as pd
 ## Load the pre-trained model and test it
 clf = NameClassifier()
 
-x_train, x_test, y_train, y_test = clf.load_data('data/multi_class_more.csv')
+x_train, x_test, y_train, y_test = clf.load_data('data/multi_class.csv')
 clf.train(x_train, y_train)
 print('acc: ', clf.evaluate(x_test, y_test))
 pred = clf.predict(x_test)
