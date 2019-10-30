0c39a1ca7b9b955783fdf020995594d84bdf4f86
Wataru Takahashi <universe3523@gmail.com>
2019-10-29 17:22:37 -0700

    support for multicharacter names

--- data/create_data.py
+++ data/create_data.py
@@ -3,19 +3,40 @@ This script creates fake name data for
 - Japanese name
 - non_japanese
 with Faker library
+
+USE THIS IN CONDA's BASE environment
 '''
 from faker import Faker
 import pandas as pd
 
-def create_jp_name(num=100, fileName='jp_names.csv'):
+def create_jp_name(num=100, fileName='jp_names.csv', mix_char=False):
+    '''
+    generate fake japanse name.
+    Param:
+        num: numbers of names to generate
+        fileName: filename to save the result on
+        mix_char: Generate Kanji, Katakana and Roman letter repsetented names if True. Kanji Only otherwise
+    '''
 
     fake = Faker('jp_JP')
     
     f = open(fileName, "w")
     f.write('code,name\n')
-    for _ in range(num):
-        name = fake.name()
-        f.write("{},{}\n".format('jp_JP', name))
+
+    if mix_char == False:
+        for _ in range(num):
+            name = fake.name()
+            f.write("{},{}\n".format('jp_JP', name))
+    elif mix_char == True:
+        for _ in range(int(num/3)):
+            name = fake.name()
+            f.write("{},{}\n".format('jp_JP', name))
+            romanized_name = ' '.join(fake.romanized_name().split(' ')[::-1])
+            f.write("{},{}\n".format('jp_JP', romanized_name))
+            kata_name = fake.kana_name()
+            f.write("{},{}\n".format('jp_JP', kata_name))
+            print('.', end='')
+
     f.close()
 
 
@@ -48,6 +69,8 @@ def create_foreign_name(loc='None', num=100, country_list='region_list.txt', fil
             for _ in range(num):
                 name = fake.name()
                 f.write("{},{} {}\n".format(country, name[0], name[1:]))
+            if _%100 == 0:
+                print('.', end='')
         else:
             for _ in range(num):
                 name = fake.name()
